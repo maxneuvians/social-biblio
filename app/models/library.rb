da05d44354c8edde3546326b816f8c406295e1e6
Max Neuvians <max@neuvians.net>
2014-05-25 14:06:43 -0400

    Crappy group by max implementation

--- app/models/library.rb
+++ app/models/library.rb
@@ -3,4 +3,13 @@ class Library < ActiveRecord::Base
   validates :twitter_id,  :presence => true, :uniqueness => true
   validates :username,    :presence => true, :uniqueness => true
 
+  def self.rank_by_followers
+    rank = {}
+    pluck(:username).each do |username|
+      tweet = Tweet.find_by(:username => username)
+      rank[username] = tweet.followers if tweet
+    end
+    rank.sort_by{|k,v|v}.reverse
+  end
+
 end
